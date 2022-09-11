bfa9f0d0fa515cbf46f1a35faa8cd4723fd70079
Christopher Díaz <nealthmc@gmail.com>
2022-09-11 07:14:40 -0500

    save user method on user controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function successResponse(mixed $data, int $code = 200): JsonResponse
+    {
+        return response()->json(['success'=> true, 'data' => $data], $code);
+    }
 }
