5561cc7a4a541b81a270e18596aa9387c44c419e
Christopher Díaz <nealthmc@gmail.com>
2022-09-11 06:06:28 -0500

    add users table

---
+++ database/migrations/2022_09_11_105618_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $this->string('name');
+            $this->string('favorite_color');
+            $this->string('email')->unique();
+            $this->date('birthday');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
